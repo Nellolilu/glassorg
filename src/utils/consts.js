e616b31a854b444712574a3ed15086e926181f1d
nellolilu <nelli.neb@gmail.com>
2021-06-03 19:03:43 +0200

    cleaned unused vars

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,5 +1,4 @@
 export const ACCESS_TOKEN = "access_token";
-const APP_NAME = "p3";
 
 const URL = process.env.REACT_APP_SERVER_URL;
 export const SERVER_URL = URL ? `${URL}` : `http://localhost:5005/api`;
