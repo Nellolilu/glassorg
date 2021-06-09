e0da43be2ad1a531146c2e2875a25c263522c3ac
nellolilu <nelli.neb@gmail.com>
2021-06-09 09:37:22 +0200

    still working on create

--- src/pages/CreatePage.jsx
+++ src/pages/CreatePage.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import axios from "axios";
+
 import "../App.css";
 import { Link } from "react-router-dom";
 import * as PATHS from "../utils/paths";
@@ -6,14 +8,53 @@ import * as CREATE_SERVICE from "../services/create";
 import * as CONSTS from "../utils/consts";
 
 export default function CreatePage(props) {
+  // SET FORM
   const [form, setForm] = React.useState({
+    name: "",
     url: "",
     email: "",
     adress: "",
-    size: "",
     description: "",
+    size: "",
+    branch: "Production",
+    // WHY CANT THIS BE EMPTY; HOW TO GET THIS OPTIONAL
+
+    // // CREATE SINGLE ANSWER ID
+    // answer: "",
+    // // LINK TO QUESTION
+    // question: "",
   });
 
+  // // GET THE BRANCHES & THE QUESTIONS
+  const [listOfQuestions, setListOfQuestions] = React.useState([]);
+  const [listOfBranches, setListOfBranches] = React.useState([]);
+  const [listOfSizes, setListOfSizes] = React.useState([]);
+  const [answers, setAnswers] = React.useState({});
+  console.log(answers);
+
+  React.useEffect(() => {
+    console.log("React use Effect");
+    axios
+      .get(`${CONSTS.SERVER_URL}/create-company`)
+      .then((response) => {
+        console.log("response:", response);
+        setListOfBranches(response.data.allBranches);
+        setListOfQuestions(response.data.allQuestions);
+        setListOfSizes(response.data.size);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  // FILTER THE QUESTIONS PER SELECTED BRANCH
+  const branchFilter = listOfBranches.find((el) => el.branch === form.branch);
+  const allFilteredQuestions = listOfQuestions.filter(
+    (el) => el.branch === branchFilter._id
+  );
+  // console.log("QUESTIONS FILTERED", allFilteredQuestions);
+
+  // HANDLE SUBMIT
   function handleChange(event) {
     setForm({ ...form, [event.target.name]: event.target.value });
   }
@@ -25,14 +66,15 @@ export default function CreatePage(props) {
     CREATE_SERVICE.CREATE_COMPANY(form, accessToken)
       .then((res) => {
         console.log("response on handlesubmit", res);
-        // WHAT IS THAT AGAIN??
+        // WHAT IS THAT AGAIN, props.history.push??
         props.history.push(`${PATHS.HOMEPAGE}`);
       })
       .catch((err) => {
         console.error("err:", err.res);
       });
-    //   API CALL TO CREATE MOVIE HERE+
   }
+
+  // RETURNING FORM
 
   return (
     <div>
@@ -45,6 +87,14 @@ export default function CreatePage(props) {
             <h4>listing of your company</h4>
             <form onSubmit={handleSubmit}>
               <div>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="company name"
+                  onChange={handleChange}
+                  value={form.name}
+                />{" "}
+                <br />
                 <input
                   type="url"
                   name="url"
@@ -68,15 +118,7 @@ export default function CreatePage(props) {
                   onChange={handleChange}
                   value={form.adress}
                 />{" "}
-                <br />{" "}
-                <input
-                  type="text"
-                  name="size"
-                  placeholder="pick a size"
-                  onChange={handleChange}
-                  value={form.size}
-                />{" "}
-                <br />{" "}
+                <br />
                 <input
                   type="text"
                   name="description"
@@ -84,53 +126,78 @@ export default function CreatePage(props) {
                   onChange={handleChange}
                   value={form.description}
                 />
+                <br />{" "}
+                <label htmlFor="">
+                  please select a company size
+                  <select
+                    name="size"
+                    type="text"
+                    onChange={handleChange}
+                    value={form.size}
+                  >
+                    {/* OPTION ISNT ON TOP */}
+                    {/* <option disabled selected>
+                      pick one
+                    </option> */}
+                    {listOfSizes.map((oneSize, index) => {
+                      return <option key={index}>{oneSize}</option>;
+                    })}
+                  </select>
+                </label>{" "}
+                <br />
+                <label htmlFor="">
+                  please select a branch
+                  <select
+                    name="branch"
+                    onChange={handleChange}
+                    value={form.branch}
+                  >
+                    {/* OPTION ISNT ON TOP */}
+                    {/* <option disabled selected>
+                      pick one
+                    </option> */}
+                    {listOfBranches.map((oneBranch) => {
+                      return (
+                        <option key={oneBranch._id}>{oneBranch.branch}</option>
+                      );
+                    })}
+                  </select>
+                </label>{" "}
+                <br />
+                <label htmlFor="">
+                  HERE COMES THE QUESTIONS
+                  <div>
+                    {allFilteredQuestions.map((oneQuestion) => {
+                      return (
+                        <div key={oneQuestion._id}>
+                          <p>{oneQuestion.question}</p>
+                          <input
+                            type="text"
+                            name="answer"
+                            placeholder={oneQuestion.placeholder}
+                            onChange={(event) => {
+                              // console.log(event.target.value);
+                              // console.log(oneQuestion);
+                              setAnswers({
+                                ...answers,
+                                [oneQuestion._id]: event.target.value,
+                              });
+                            }}
+                            value={answers[oneQuestion._id] || ""}
+                          />
+                          <br />{" "}
+                        </div>
+                      );
+                    })}
+                  </div>
+                </label>
               </div>
               <button type="submit">Create this listing</button>
             </form>
+            {/* <a class="home-btn" href="/">go home</a> */}
           </section>
         </>
       )}
     </div>
   );
 }
-
-//   <input class="form-input" placeholder= "company name" name="name" value="{{name}}">
-//   <input class="form-input" placeholder= "webpage" type="url" name="url" value="{{url}}">
-//   <input class="form-input" placeholder= "email" type="email" name="email" value="{{email}}">
-//   <input class="form-input" placeholder= "adress" name="adress" value="{{adress}}">
-//     <select class="form-input" name="size" id="size" >
-//     <option disabled selected>company size</option>
-//     {{#each size}}
-//     <option value="{{this}}">{{this}}</option>
-//     {{/each}}
-//   </select>
-//     <select class="form-input" name="branch" id="branch" >
-//       <option disabled selected>branch</option>
-//     {{#each branch}}
-//     <option value="{{this}}">{{this}}</option>
-//     {{/each}}
-//   </select>
-//   <input class="form-input" placeholder= "describe your company" name="description" value="{{description}}">
-//   {{!-- <input class="form-input" type="file" name="image"> --}}
-// <br>
-// <label for="file-upload" class="file-upload">
-//     upload a logo
-// </label>
-// <input id="file-upload" type="file" name="image"/>
-
-// <h4>and go transparent</h4>
-// <h6 class="description">Fill out this questionaire and share your sustainablilty approaches.<br>To make your profile trustworthy, you can upload pictures that proof<br>your statements after you succesfully listed your company.</h6>
-//   <p><strong>social question / </strong>do you have more male or more female positions in leading roles of your business?</p>
-//   <input class="form-input" placeholder= "male / female / equal" name="social1" value="{{social1}}">
-// <p><strong>economic question / </strong>do you pay taxes on your product/service where your costumer is based?</p>
-//   <input class="form-input" placeholder= "yes / no / some %" name="ecological1" value="{{ecological1}}">
-//   <p><strong>ecological question / </strong>is your company running completely on renewable energy?</p>
-//   <input class="form-input" placeholder= "yes / no / some %" name="economic1" value="{{economic1}}">
-//     <p><strong>ecological question / </strong>what percentage of the CO2 your business produces are you compensating?</p>
-//   <input class="form-input" placeholder= "some %" name="ecological2" value="{{ecological2}}">
-// <br>
-// <a class="create-btn" href="javascript:;" onclick="parentNode.submit();">create listing</a>
-
-// </form>
-
-// <a class="home-btn" href="/">go home</a>
